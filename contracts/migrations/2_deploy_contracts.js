c98f9c0757639221d962a046aed29cd26d4e0597
Stoyan Tushinov <tanuka@mail.bg>
2018-07-11 11:56:58 +0300

    Removed 2_deploy_contracts.js

--- contracts/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const FUNDING_FACTORY = artifacts.require("FundingFactory");
-const FUND = artifacts.require("Fund");
-
-module.exports = function(deployer){
-    deployer.deploy(FUNDING_FACTORY);
-    deployer.deploy(FUND);
-};
